212a2bcecc6c0184951fed2b589e36742119f2b3
KimeT <kimmo.tahvanainen@gmail.com>
2016-06-16 00:06:16 +0300

    Dim button added

--- planmill_request_tweak.user.js
+++ planmill_request_tweak.user.js
@@ -3,9 +3,9 @@
 // @namespace    Planmill
 // @description  Customizations to PlanMill UI
 // @downloadURL  https://github.com/KimeT/PlanMill-Tweaks/raw/master/planmill_request_tweak.user.js
-// @icon         https://online.planmill.com/javerdel/generic/pictures/favicon.ico
-// @match        https://online.planmill.com/javerdel/index.jsp?category=Sales%20management.Requests
-// @version      0.0.2
+// @icon         https://online.planmill.com/pmtrial/generic/login/favicon.ico
+// @match        https://online.planmill.com/*/index.jsp?category=Sales%20management.Requests
+// @version      0.0.3
 // @author       KimeT
 // @homepage     https://github.com/KimeT/PlanMill-Tweaks
 // @grant        none
@@ -14,74 +14,66 @@
 
 // FUNCTIONS
 
-// DIM NON-ACCOUNT ROWS
-function dimNonAccountRows() {
+// SET DIM ON NON-ACCOUNT ROWS
+function setDimNonAccountRows() {
 	var yIndex;
+	var opacity = $('#tools-dim-nonaccount-rows').hasClass('ui-state-active') ? '0.5' : '';
+
+	// Find Account column
 	$('#data table th a').each(function () {
 		if ($(this).text() === "Yritys" || $(this).text() === "Account" || $(this).text() === "Firma") {
 			yIndex = $(this).closest('th').index(); return false;
 		}
 	});
 
+	// Set opacity to rows, where Account column is empty
 	if (yIndex !== undefined) {
 		$('#data table tr').each(function () {
 			if ($(this).children('td:nth(' + yIndex + ')').text() === '') {
-				$(this).closest('tr').css('opacity', '0.5');
+				$(this).closest('tr').css('opacity', opacity);
 			}
 		});
 	}
 }
 
-// UNDIM NON-ACCOUNT ROWS
-function undimNonAccountRows() {
-	var yIndex;
-	$('#data table th a').each(function () {
-		if ($(this).text() === "Yritys" || $(this).text() === "Account" || $(this).text() === "Firma") {
-			yIndex = $(this).closest('th').index(); return false;
-		}
-	});
-
-	if (yIndex !== undefined) {
-		$('#data table tr').each(function () {
-			if ($(this).children('td:nth(' + yIndex + ')').text() === '') {
-				$(this).closest('tr').css('opacity', '');
-			}
-		});
-	}
+// TOGGLE DIM BUTTON STATE
+function toggleDimButtonState() {
+	$('#tools-dim-nonaccount-rows').toggleClass('ui-state-active');
+	setDimNonAccountRows();
 }
 
 // ADD DIM BUTTON
 function addDimButton() {
-	var $pDiv = $('<div>');
+	var headerText = $('#top-bar-subject').text().trim().toLowerCase();
+	var btnTexts = { fi: 'Yrityksettömien rivien himmennys', en: 'Dimming of accountless rows' };
+	var $div = $('<div>');
 	var $btn = $('<button type="button" id="tools-dim-nonaccount-rows" class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-icon-primary" role="button"></button>');
-	$btn.append($('<span class="ui-button-icon-primary ui-icon ui-icon-cancel"></span>'));
-	$btn.append($('<span class="ui-button-text">Himmennä yrityksettömät rivit</span>'));
-	$btn.click(dimNonAccountRows);
-	$btn.click(function () {
-		$(this).toggleClass('ui-state-active');
-  });
+	var $span = $('<span class="ui-button-icon-primary ui-icon ui-icon-cancel"></span>');
+	$btn.append($span);
+	$span = $('<span class="ui-button-text"></span>');
+	switch (headerText) {
+		case 'requests':
+			$span.text(btnTexts.en);
+			break;
+		case 'palvelupyynnöt':
+		default:
+			$span.text(btnTexts.fi);
+	}
+	$btn.append($span);
+
+	$btn.click(toggleDimButtonState);
 	$btn.hover(
 		function () {
 			$(this).addClass('ui-state-hover');
 		}, function () {
 			$(this).removeClass('ui-state-hover');
-  	}
+		}
 	);
 
-	$pDiv.append($btn);
-	$('#tools-content-left-links').append($pDiv);
+	$div.append($btn);
+	$('#tools-content-left-links').append($div);
 }
 
 // ACTIONS
-//addDimButton();
-dimNonAccountRows();
-
-//$('#paging-pages button').on("click", dimNonAccountRows);
-
-//$('#paging-pages button').on("click", function () { alert('Clicked'); });
-
-/*$(document).ajaxComplete(function(event, request, settings) {
-	alert('Ajax completed!');
-});*/
-
-$(document).ajaxComplete(dimNonAccountRows);
+addDimButton();
+$(document).ajaxComplete(setDimNonAccountRows);
